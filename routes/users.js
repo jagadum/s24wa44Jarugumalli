c666be1349f3b8f159fd70092d44bcd1230c9e48
jagadum <jarugumallinuthan@gmail.com>
2024-03-13 14:42:33 -0500

    users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Initialize variables
+var product = 1;
+var factor = 1;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Increment factor and multiply product
+  factor++;
+  product *= factor;
+
+  // Send response with updated product value
+  res.send(`Product is: ${product}`);
 });
 
 module.exports = router;
