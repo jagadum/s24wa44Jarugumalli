03a1550f6299211beb99b70a15f9eacd5f628e5c
jagadum <jarugumallinuthan@gmail.com>
2024-03-13 13:27:55 -0500

    new endpoint

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var mydataRouter = require('./routes/mydata');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter = require('./routes/mydata'); // Add this line for mydataRouter
 
 var app = express();
 
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);
+app.use('/mydata', mydataRouter); // Add this line to use mydataRouter
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
