d0ee3cb06d00b48fee9c290d0423f417cf47fe24
jagadum <jarugumallinuthan@gmail.com>
2024-03-13 13:25:17 -0500

    new endpoint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # s24wa44Jarugumalli 
 Hii Every one
+
+link to my remote Render server: https://s24wa44jarugumalli.onrender.com/
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
