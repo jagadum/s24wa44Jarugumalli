fa9bc8c981b04ee259b463f701007e52696da1f1
jagadum <jarugumallinuthan@gmail.com>
2024-03-18 19:28:26 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -2,24 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  // Check if the request has a query argument associated with x
+
   let x = req.query.x;
 
-  // If x is provided in the query, parse it as a number
+  
   if (x) {
     x = parseFloat(x);
   } else {
-    // Generate a random value between 1 and 10 if x is not provided
+    
     x = Math.floor(Math.random() * 10) + 1;
   }
 
-  // Apply Math.pow() function to the random value
-  let result = Math.pow(x, 2); // Squaring the value for example
+  
+  let y = req.query.y;
 
-  // Construct the response string
-  let response = `Math.pow() applied to ${x} is ${result}`;
+  
+  if (y) {
+    y = parseFloat(y);
+  } else {
 
-  // Send the response
+    y = Math.floor(Math.random() * 10) + 1;
+  }
+
+  let result = Math.pow(x, y);
+
+  let response = `Math.pow() applied to ${x} and ${y} is ${result}`;
+
   res.send(response);
 });
 
