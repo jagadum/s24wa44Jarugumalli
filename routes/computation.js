d84a2fa7c3ee9e7ed34ba83d36b399038594611a
jagadum <jarugumallinuthan@gmail.com>
2024-03-18 19:36:11 -0500

    bonus completed

--- routes/computation.js
+++ routes/computation.js
@@ -2,20 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-
   let x = req.query.x;
-
-  
   if (x) {
     x = parseFloat(x);
   } else {
     
     x = Math.floor(Math.random() * 10) + 1;
   }
-
-  
   let y = req.query.y;
-
   
   if (y) {
     y = parseFloat(y);
@@ -23,7 +17,6 @@ router.get('/', function(req, res, next) {
 
     y = Math.floor(Math.random() * 10) + 1;
   }
-
   let result = Math.pow(x, y);
 
   let response = `Math.pow() applied to ${x} and ${y} is ${result}`;
