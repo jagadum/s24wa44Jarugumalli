e66f676753be15a68e464a39aeb1d2378244c128
jagadum <jarugumallinuthan@gmail.com>
2024-03-20 18:12:58 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -6,20 +6,27 @@ router.get('/', function(req, res, next) {
   if (x) {
     x = parseFloat(x);
   } else {
-    
     x = Math.floor(Math.random() * 10) + 1;
   }
+
   let y = req.query.y;
-  
   if (y) {
     y = parseFloat(y);
   } else {
-
     y = Math.floor(Math.random() * 10) + 1;
   }
-  let result = Math.pow(x, y);
 
-  let response = `Math.pow() applied to ${x} and ${y} is ${result}`;
+  let powResult = Math.pow(x, y);
+  let signResult = Math.sign(x);
+  let tanhResult = Math.tanh(x);
+  let truncResult = Math.trunc(x);
+
+  let response = `
+    Math.pow() applied to ${x} and ${y} is ${powResult}<br>
+    Math.sign() applied to ${x} is ${signResult}<br>
+    Math.tanh() applied to ${x} is ${tanhResult}<br>
+    Math.trunc() applied to ${x} is ${truncResult}<br>
+  `;
 
   res.send(response);
 });
